ef63cf0f04136f291ca93fa5a0abb5e7d57718be
matiri132 <matias@craftech.io>
2022-07-04 12:22:09 -0300

    chore: agrego presentacion y recursos para clase2

--- clase2/homework/1-api-django/backend/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from api.models import Recordatorio
-
-class RecordatorioSerializer(ModelSerializer):
-    class Meta:
-        model = Recordatorio
-        fields = ['detalle']
--- clase2/homework/1-api-django/backend/api/views.py
+++
@@ -1,9 +0,0 @@
-
-from rest_framework.viewsets import ModelViewSet
-from api.serializers import RecordatorioSerializer
-from api.models import Recordatorio
-
-
-class ApiViewSet(ModelViewSet):
-    serializer_class = RecordatorioSerializer
-    queryset = Recordatorio.objects.all()
